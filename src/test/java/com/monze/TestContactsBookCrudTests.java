07ac2cf9f330c7d8d00c38330c7b5181349e1ab2
Linos Nchena <linosnchena@gmail.com>
2020-09-14 13:25:08 +0200

    third unit Testing in Mockito Testing

--- src/test/java/com/monze/TestContactsBookCrudTests.java
+++ src/test/java/com/monze/TestContactsBookCrudTests.java
@@ -15,6 +15,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 import static org.mockito.Mockito.*;
 
 public class TestContactsBookCrudTests {
@@ -58,33 +59,50 @@ public class TestContactsBookCrudTests {
         assertEquals(0 + 3, contactsDatabase.numberOfContacts() + 3);
     }
 
-    //  @Test
-    //  public void testContactSearchByName() throws SQLException {
-    //      // When there is a contacts saved as LEON
-    //      String expectedContacts = "LEON";
-    //      //int expectedContacts1 = 1;
-    //   //   String sql1 = "DROP TABLE IF EXISTS tblphone;";
-    //   //   String sql2 = "CREATE TABLE tblphone (id SERIAL PRIMARY KEY,pname TEXT NOT NULL, pnumber1 INT NOT NULL,pnumber2 INT);";
-    //      String sql3 = "INSERT INTO tblphone(pname,pnumber1,pnumber2) VALUES('LEON',8888,21);";
-    //   //   when(connection.prepareStatement(sql1)).thenReturn(preparedStatement);
-    //   //   when(preparedStatement.executeUpdate(sql1));
-    //    //  when(connection.prepareStatement(sql2)).thenReturn(preparedStatement);
-    //   //   when(preparedStatement.executeUpdate(sql2));
-    //      when(connection.prepareStatement(sql3)).thenReturn(preparedStatement);   
-    //      when(preparedStatement.executeUpdate(sql3));
+    @Test
+    public void testSearchContactByCorrectName() throws SQLException {
+        // When there is a contacts saved as LEON
+        String pnamex = "LEON";
+        String pnamez = "LEONx";
+        String sql1 = "INSERT INTO tblphone(pname,pnumber1,pnumber2) VALUES('LEON',8822288,299991);";
+        String sql3 = "SELECT * FROM  tblphone WHERE  pname ='" + pnamex + "'";
 
-    //      when(preparedStatement.executeQuery()).thenReturn(resultSet);
-    //      when(resultSet.next()).thenReturn(true);
-    //      when(resultSet.getString(2)).thenReturn(expectedContacts);
-    //     // when(resultSet.getInt(1)).thenReturn(expectedContacts);
+        when(connection.prepareStatement(sql1)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
 
-    // //     /// Tests
-    //      assertEquals(expectedContacts, contactsDatabase.findData("LEON").getString(2));
-    //      verify(resultSet).getString(2);
-    //      verify(resultSet).getInt(1);
-    // //     // When there are not any contacts
-    //      when(resultSet.next()).thenReturn(false);
-    //      assertEquals(expectedContacts, contactsDatabase.findData("LEONx").getString(2));
-    //  }
+        when(connection.prepareStatement(sql3)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getString(2)).thenReturn(pnamex);
+        String result1 = resultSet.getString(2);
+
+        // Trials
+        assertEquals(pnamex, result1);
+        assertNotEquals(pnamez, result1);
+    }
+
+    @Test
+    public void testSearchContactByWrongName() throws SQLException {
+        // When there is a contacts saved as LEON
+        String pnamex = "LEON";
+        String pnamez = "LEONx";
+        String sql1 = "INSERT INTO tblphone(pname,pnumber1,pnumber2) VALUES('LEON',8822288,299991);";
+        String sql3 = "SELECT * FROM  tblphone WHERE  pname ='" + pnamex + "'";
+
+        when(connection.prepareStatement(sql1)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+
+        when(connection.prepareStatement(sql3)).thenReturn(preparedStatement);
+        when(preparedStatement.executeQuery()).thenReturn(resultSet);
+
+        when(resultSet.next()).thenReturn(true);
+        when(resultSet.getString(2)).thenReturn(pnamex);
+        String result1 = resultSet.getString(2);
+
+        // Trials
+        assertEquals(pnamex, result1);
+        assertNotEquals(pnamez, result1);
+    }
 
 }
