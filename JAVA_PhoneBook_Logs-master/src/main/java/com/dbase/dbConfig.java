1a3acb526f63e0f026feaf1111f4d352f1b0b2e2
LINOS NCHENA <linosnchena@gmail.com>
2020-09-23 16:11:51 +0200

    consolidating reset1 and reset2 functions

--- JAVA_PhoneBook_Logs-master/src/main/java/com/dbase/dbConfig.java
+++
@@ -1,9 +0,0 @@
-package com.dbase;
-
-public class dbConfig {
-
-  public static final String url = "jdbc:postgresql://localhost:5432/presly";
-  public static final String user1 = "postgres";
-  public static final String passwd1 = "Monze@2019";
-
-}
